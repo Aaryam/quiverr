4fd27db7f215285a9a8e21dbc024d6104fb45302
Aaryan And Aaman <aaryanaaman@Aaryans-iMac.local>
2020-03-17 13:03:59 +0530

    Fixed bug

--- main.js
+++ main.js
@@ -17,3 +17,9 @@ function returnImageClothes ()
     document.getElementById("myImg").src = imageSource;
     console.log(imageSource);
 }
+
+/* Commands to commit */
+
+/* 1. git add * */
+/* 2. git commit -m "msg" */
+/* 3. git push origin master */
